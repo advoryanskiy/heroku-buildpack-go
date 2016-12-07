6b40759c79a820d4c98d599dfb221bdeff15b858
Edward Muller <edwardam@interlix.com>
2016-12-07 11:06:07 -0800

    WIP

--- lib/common.sh
+++ lib/common.sh
@@ -5,7 +5,7 @@
 # allow apps to specify cgo flags. The literal text '${build_dir}' is substituted for the build directory
 
 if [ -z "${buildpack}" ]; then
-  buildpack=$(cd "$(dirname $0)/.." && pwd)
+    buildpack=$(cd "$(dirname $0)/.." && pwd)
 fi
 
 DataJSON="${buildpack}/data.json"
@@ -28,172 +28,143 @@ GO_LINKER_VALUE=${SOURCE_VERSION}
 
 
 warn() {
-  echo -e "${YELLOW} !!    $@${NC}"
+    echo -e "${YELLOW} !!    $@${NC}"
 }
 
 err() {
-  echo -e >&2 "${RED} !!    $@${NC}"
+    echo -e >&2 "${RED} !!    $@${NC}"
 }
 
 step() {
-  echo "$steptxt $@"
+    echo "$steptxt $@"
 }
 
 start() {
-  echo -n "$steptxt $@... "
+    echo -n "$steptxt $@... "
 }
 
 finished() {
-  echo "done"
-}
-
-downloadJQ() {
-  local JQDir="${1}"
-  mkdir -p ${JQDir}
-  pushd "${JQDir}" &> /dev/null
-    start "Fetching jq"
-      ${CURL} -O "${BucketURL}/jq-linux64"
-      mv "jq-linux64" "jq"
-      chmod a+x jq
-    finished
-  popd &> /dev/null
-}
-
-ensureJQ() {
-  local JQDir="${1}"
-  local JQBin="${JQDir}/jq"
-  if echo "${PATH}" | grep -v "${JQDir}" &> /dev/null; then
-    PATH="${JQDir}:${PATH}"
-  fi
-  if [ ! -x "${JQBin}" ]; then
-    downloadFile "jq-linux64" "${JQDir}"
-  fi
-  local sw="$(< "${FilesJSON}" jq -r '."jq-linux64".SHA')"
-  local sh="$(shasum -a256 "${JQBin}" | cut -d \  -f 1)"
-  if [ "${sw}" != "${sh}" ]; then
-    rm -f "${JQBin}"
-    downloadFile "jq-linux64" "${JQDir}"
-  fi
+    echo "done"
 }
 
 determinLocalFileName() {
-  local fileName="${1}"
-  local localName="jq"
-  if [ "${fileName}" != "jq-linux64" ]; then #jq is special cased here because we can't jq until we have jq'
-    localName="$(< "${FilesJSON}" jq -r '."'${fileName}'".LocalName | if . = null then "'${fileName}'" else . end')"
-  fi
-  echo "${localName}"
+    local fileName="${1}"
+    local localName="jq"
+    if [ "${fileName}" != "jq-linux64" ]; then #jq is special cased here because we can't jq until we have jq'
+        localName="$(< "${FilesJSON}" jq -r '."'${fileName}'".LocalName | if . = null then "'${fileName}'" else . end')"
+    fi
+    echo "${localName}"
 }
 
 ensureFile() {
-  local fileName="${1}"
-  local targetDir="${2}"
-  local localName="$(determinLocalFileName "${fileName}")"
-  local targetBin="${targetDir}/${localName}"
-  if echo "${PATH}" | grep -v "${targetDir}" &> /dev/null; then
-    PATH="${targetDir}:${PATH}"
-  fi
-  if [ ! -x "${targetBin}" ]; then
-    downloadFile "${fileName}" "${targetDir}"
-  fi
-  local sw="$(< "${FilesJSON}" jq -r '."'${fileName}'".SHA')"
-  local sh="$(shasum -a256 "${targetBin}" | cut -d \  -f 1)"
-  if [ "${sw}" != "${sh}" ]; then
-    rm -f "${targetBin}"
-    downloadFile "${fileName}" "${targetDir}"
-  fi
+    local fileName="${1}"
+    local targetDir="${2}"
+    local localName="$(determinLocalFileName "${fileName}")"
+    local targetBin="${targetDir}/${localName}"
+    if echo "${PATH}" | grep -v "${targetDir}" &> /dev/null; then
+        PATH="${targetDir}:${PATH}"
+    fi
+    if [ ! -x "${targetBin}" ]; then
+        downloadFile "${fileName}" "${targetDir}"
+    fi
+    local sw="$(< "${FilesJSON}" jq -r '."'${fileName}'".SHA')"
+    local sh="$(shasum -a256 "${targetBin}" | cut -d \  -f 1)"
+    if [ "${sw}" != "${sh}" ]; then
+        rm -f "${targetBin}"
+        downloadFile "${fileName}" "${targetDir}"
+    fi
 }
 
 downloadFile() {
-  local fileName="${1}"
-  local targetDir="${2}"
-  local localName="$(determinLocalFileName "${fileName}")"
+    local fileName="${1}"
+    local targetDir="${2}"
+    local localName="$(determinLocalFileName "${fileName}")"
 
-  mkdir -p "${targetDir}"
-  pushd "${targetDir}" &> /dev/null
-    start "Fetching ${localName}"
-      ${CURL} -O "${BucketURL}/${fileName}"
-      if [ "${fileName}" != "${localName}" ]; then
-        mv "${fileName}" "${localName}"
-      fi
-      chmod a+x ${localName}
-    finished
-  popd &> /dev/null
+    mkdir -p "${targetDir}"
+    pushd "${targetDir}" &> /dev/null
+        start "Fetching ${localName}"
+            ${CURL} -O "${BucketURL}/${fileName}"
+            if [ "${fileName}" != "${localName}" ]; then
+                mv "${fileName}" "${localName}"
+            fi
+            chmod a+x ${localName}
+        finished
+    popd &> /dev/null
 }
 
 loadEnvDir() {
-  local env_dir="${1}"
-  if [ ! -z "${env_dir}" ]; then
-    mkdir -p "${env_dir}"
-    env_dir=$(cd "${env_dir}/" && pwd)
-    for key in CGO_CFLAGS CGO_CPPFLAGS CGO_CXXFLAGS CGO_LDFLAGS GO_LINKER_SYMBOL GO_LINKER_VALUE GO15VENDOREXPERIMENT GOVERSION GO_INSTALL_PACKAGE_SPEC GO_INSTALL_TOOLS_IN_IMAGE GO_SETUP_GOPATH_IN_IMAGE; do
-      if [ -f "${env_dir}/${key}" ]; then
-        export "${key}=$(cat "${env_dir}/${key}" | sed -e "s:\${build_dir}:${build}:")"
-      fi
-    done
-  fi
+    local env_dir="${1}"
+    if [ ! -z "${env_dir}" ]; then
+        mkdir -p "${env_dir}"
+        env_dir=$(cd "${env_dir}/" && pwd)
+        for key in CGO_CFLAGS CGO_CPPFLAGS CGO_CXXFLAGS CGO_LDFLAGS GO_LINKER_SYMBOL GO_LINKER_VALUE GO15VENDOREXPERIMENT GOVERSION GO_INSTALL_PACKAGE_SPEC GO_INSTALL_TOOLS_IN_IMAGE GO_SETUP_GOPATH_IN_IMAGE; do
+            if [ -f "${env_dir}/${key}" ]; then
+                export "${key}=$(cat "${env_dir}/${key}" | sed -e "s:\${build_dir}:${build}:")"
+            fi
+        done
+    fi
 }
 
 setGoVersionFromEnvironment() {
-  if [ -z "${GOVERSION}" ]; then
-    warn ""
-    warn "'GOVERSION' isn't set, defaulting to '${DefaultGoVersion}'"
-    warn ""
-    warn "Run 'heroku config:set GOVERSION=goX.Y' to set the Go version to use"
-    warn "for future builds"
-    warn ""
-  fi
-  ver=${GOVERSION:-$DefaultGoVersion}
+    if [ -z "${GOVERSION}" ]; then
+        warn ""
+        warn "'GOVERSION' isn't set, defaulting to '${DefaultGoVersion}'"
+        warn ""
+        warn "Run 'heroku config:set GOVERSION=goX.Y' to set the Go version to use"
+        warn "for future builds"
+        warn ""
+    fi
+    ver=${GOVERSION:-$DefaultGoVersion}
 }
 
 determineTool() {
-  if [ -f "${godepsJSON}" ]; then
-    TOOL="godep"
-    step "Checking Godeps/Godeps.json file."
-    if ! jq -r . < "${godepsJSON}" > /dev/null; then
-      err "Bad Godeps/Godeps.json file"
-      exit 1
+    if [ -f "${godepsJSON}" ]; then
+        TOOL="godep"
+        step "Checking Godeps/Godeps.json file."
+        if ! jq -r . < "${godepsJSON}" > /dev/null; then
+            err "Bad Godeps/Godeps.json file"
+        exit 1
+        fi
+        name=$(<${godepsJSON} jq -r .ImportPath)
+        ver=${GOVERSION:-$(<${godepsJSON} jq -r .GoVersion)}
+        warnGoVersionOverride
+    elif [ -f "${vendorJSON}" ]; then
+        TOOL="govendor"
+        step "Checking vendor/vendor.json file."
+        if ! jq -r . < "${vendorJSON}" > /dev/null; then
+            err "Bad vendor/vendor.json file"
+            exit 1
+        fi
+        name=$(<${vendorJSON} jq -r .rootPath)
+        if [ "$name" = "null" -o -z "$name" ]; then
+            err "The 'rootPath' field is not specified in 'vendor/vendor.json'."
+            err "'rootPath' must be set to the root package name used by your repository."
+            err "Recent versions of govendor add this field automatically, please upgrade"
+            err "and re-run 'govendor init'."
+            err ""
+            err "For more details see: https://devcenter.heroku.com/articles/go-apps-with-govendor#build-configuration"
+            exit 1
+        fi
+        ver=${GOVERSION:-$(<${vendorJSON} jq -r .heroku.goVersion)}
+        warnGoVersionOverride
+        if [ "${ver}" =  "null" -o -z "${ver}" ]; then
+            ver=${DefaultGoVersion}
+            warn "The 'heroku.goVersion' field is not specified in 'vendor/vendor.json'."
+            warn ""
+            warn "Defaulting to ${ver}"
+            warn ""
+            warn "For more details see: https://devcenter.heroku.com/articles/go-apps-with-govendor#build-configuration"
+            warn ""
+        fi
+    elif [ -f "${glideYAML}" ]; then
+        TOOL="glide"
+        setGoVersionFromEnvironment
+    elif [ -d "$build/src" -a -n "$(find "$build/src" -mindepth 2 -type f -name '*.go' | sed 1q)" ]; then
+        TOOL="gb"
+        setGoVersionFromEnvironment
+    else
+        err "Godep, GB or govendor are required. For instructions:"
+        err "https://devcenter.heroku.com/articles/go-support"
+        exit 1
     fi
-    name=$(<${godepsJSON} jq -r .ImportPath)
-    ver=${GOVERSION:-$(<${godepsJSON} jq -r .GoVersion)}
-    warnGoVersionOverride
-  elif [ -f "${vendorJSON}" ]; then
-    TOOL="govendor"
-    step "Checking vendor/vendor.json file."
-    if ! jq -r . < "${vendorJSON}" > /dev/null; then
-      err "Bad vendor/vendor.json file"
-      exit 1
-    fi
-    name=$(<${vendorJSON} jq -r .rootPath)
-    if [ "$name" = "null" -o -z "$name" ]; then
-      err "The 'rootPath' field is not specified in 'vendor/vendor.json'."
-      err "'rootPath' must be set to the root package name used by your repository."
-      err "Recent versions of govendor add this field automatically, please upgrade"
-      err "and re-run 'govendor init'."
-      err ""
-      err "For more details see: https://devcenter.heroku.com/articles/go-apps-with-govendor#build-configuration"
-      exit 1
-    fi
-    ver=${GOVERSION:-$(<${vendorJSON} jq -r .heroku.goVersion)}
-    warnGoVersionOverride
-    if [ "${ver}" =  "null" -o -z "${ver}" ]; then
-      ver=${DefaultGoVersion}
-      warn "The 'heroku.goVersion' field is not specified in 'vendor/vendor.json'."
-      warn ""
-      warn "Defaulting to ${ver}"
-      warn ""
-      warn "For more details see: https://devcenter.heroku.com/articles/go-apps-with-govendor#build-configuration"
-      warn ""
-    fi
-  elif [ -f "${glideYAML}" ]; then
-    TOOL="glide"
-    setGoVersionFromEnvironment
-  elif [ -d "$build/src" -a -n "$(find "$build/src" -mindepth 2 -type f -name '*.go' | sed 1q)" ]; then
-    TOOL="gb"
-    setGoVersionFromEnvironment
-  else
-    err "Godep, GB or govendor are required. For instructions:"
-    err "https://devcenter.heroku.com/articles/go-support"
-    exit 1
-  fi
 }
